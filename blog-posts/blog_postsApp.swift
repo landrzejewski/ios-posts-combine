0cf5fef55cd29130538e9b4d2c323facd55fd652
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-07-10 09:06:02 +0200

    Initial Commit

---
+++ blog-posts/blog_postsApp.swift
@@ -0,0 +1,17 @@
+//
+//  blog_postsApp.swift
+//  blog-posts
+//
+//  Created by Łukasz Andrzejewski on 10/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct blog_postsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
